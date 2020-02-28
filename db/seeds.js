d4ddc7b9baa4f57cc94154649130ece6c1bf3aad
Darryl Hood <darrylhood@Darryls-MacBook-Air.local>
2020-02-28 14:31:28 -0500

    seed group complete

--- db/seeds.js
+++ db/seeds.js
@@ -6,8 +6,23 @@ let newUsers = [
         lastName: 'Hood',
         email: 'darryl.r.hood@gmail.com',
         occupation:  'software engineer',
-        specializations: 'fucking off',
-        skills: '',
+        specializations: 'front-end',
+        skills: 'HTML, CSS, JavaScript',
     },
-    
+    {
+        firstName: 'Theresa',
+        lastName: 'Jones',
+        email: 'tjones@nothing.com',
+        occupation:  'microbiologist',
+        specializations: 'cellular degeneration',
+        skills: 'research, cellular degeneration mapping, degeneration reversal',
+    },
+    {
+        firstName: 'Thomas',
+        lastName: 'Dodd',
+        email: 'dr.thomas@medwatch.com',
+        occupation:  'general practitioner',
+        specializations: 'pediatrics, abnormal growth',
+        skills: 'correction of growth issues, general medicine',
+    }
 ]
